eb634454a776c751ed675a8aa275406ef52303fa
jyothi0911 <sivanagajyothi522@gmail.com>
2020-07-17 12:06:49 +0530

    task 4

---
+++ src/test/java/org/git/project/ProgectGit4.java
@@ -0,0 +1,20 @@
+package org.git.project;
+
+public class ProgectGit4 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=32;
+		
+		System.out.println("before sorting a is" +a);
+		System.out.println("before sorting b is" + b);
+		
+		int c;
+	        c=a;
+	        a=b;
+	        b=c;
+	        System.out.println("after sorting a is " +a);
+	        System.out.println("after sorting b is" +b);
+	        
+	}
+
+}
